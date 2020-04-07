b33ac3c777966b837525fc252b52652654defccb
Guoyu Ou <benogy@gmail.com>
2020-04-07 21:34:43 +0800

    add license

--- omake.lua
+++ omake.lua
@@ -1,6 +1,6 @@
 project = CreateProject()
 
-target = project:CreateLibrary("text_utils")
-target:AddSourceFile("*.cpp")
+target = project:CreateLibrary("text_utils", STATIC)
+target:AddSourceFiles("*.cpp")
 
 return project
