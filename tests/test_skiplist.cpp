526fa2002e293a8a7e8cf36b4423e24263b5155b
Guoyu Ou <benogy@gmail.com>
2024-06-06 21:17:54 +0800

    refactor skiplist

--- tests/test_skiplist.cpp
+++ tests/test_skiplist.cpp
@@ -16,23 +16,35 @@ static void TestConst(const SkipListSet<int>& sl) {
 }
 
 static void TestSkipListSet(void) {
-    cout << "----- test " << __func__ << " begin -----" << endl;
-
     SkipListSet<int> sl;
-    for (int i = 100; i > 0; i -= 10) {
-        auto ret_pair = sl.Insert(i);
+    constexpr int expected_values[] = {100, 90, 80, 70, 60, 50, 40, 30, 20, 10};
+    constexpr int nr_values = 10;
+
+    cout << "----- test insert -----" << endl;
+
+    for (int i = 0; i < nr_values; ++i) {
+        auto ret_pair = sl.Insert(expected_values[i]);
         assert(ret_pair.second);
     }
-    for (int i = 100; i > 0; i -= 10) {
-        auto ret_pair = sl.Insert(i);
+    for (auto it = sl.GetBeginIterator(); it != sl.GetEndIterator(); ++it) {
+        cout << "    " << *it << endl;
+    }
+
+    for (int i = nr_values - 1; i >= 0; --i) {
+        auto ret_pair = sl.Insert(expected_values[i]);
         assert(!ret_pair.second);
     }
 
+    cout << "----- test iterator -----" << endl;
+
     auto it = sl.GetBeginIterator();
-    for (int i = 10; i <= 100; i += 10) {
-        assert(*it == i);
+    for (int i = nr_values - 1; i >= 0; --i) {
+        assert(*it == expected_values[i]);
         ++it;
     }
+    assert(it == sl.GetEndIterator());
+
+    cout << "----- test lookup -----" << endl;
 
     TestConst(sl);
 
@@ -43,12 +55,23 @@ static void TestSkipListSet(void) {
     it = sl.Lookup(101);
     assert(it == sl.GetEndIterator());
 
-    sl.Remove(20);
+    cout << "----- test lookup greater or equal -----" << endl;
 
-    it = sl.LookupGreaterOrEqual(21);
+    it = sl.LookupGreaterEqual(21);
     assert(*it == 30);
 
+    it = sl.LookupGreaterEqual(50);
+    assert(*it == 50);
+
+    it = sl.LookupGreaterEqual(51);
+    assert(*it == 60);
+
+    cout << "----- test lookup less than -----" << endl;
+
     it = sl.LookupLessThan(21);
+    assert(*it == 20);
+
+    it = sl.LookupLessThan(20);
     assert(*it == 10);
 
     it = sl.LookupLessThan(1000);
@@ -57,28 +80,34 @@ static void TestSkipListSet(void) {
     it = sl.LookupLessThan(1);
     assert(it == sl.GetEndIterator());
 
+    cout << "----- test remove -----" << endl;
+
     int ret = 0;
-    sl.RemoveGreaterOrEqual(21, &ret);
+    sl.Remove(20, &ret);
+    assert(ret == 20);
+
+    sl.RemoveGreaterEqual(21, &ret);
     assert(ret == 30);
+
+    sl.RemoveGreaterEqual(40, &ret);
+    assert(ret == 40);
+
+    sl.RemoveGreaterEqual(99, &ret);
+    assert(ret == 100);
 
     for (auto it = sl.GetBeginIterator(); it != sl.GetEndIterator(); ++it) {
         cout << "    " << *it << endl;
     }
-    cout << "----- test " << __func__ << " end -----" << endl;
 }
 
 static void TestSkipListMap(void) {
-    cout << "----- test " << __func__ << " begin -----" << endl;
+    cout << "----- test user defined value -----" << endl;
 
     struct TestValue {
-        TestValue(int _a = 5) : a(_a) {
-            cout << "construct a = " << a << endl;
-        }
+        TestValue(int _a = 5) : a(_a) {}
         TestValue(const TestValue& t) {
             a = t.a;
-            cout << "copy construct a = " << t.a << endl;
         }
-        ~TestValue() { cout << "destroy TestValue[" << a << "]" << endl; }
         int a;
     };
 
@@ -97,8 +126,6 @@ static void TestSkipListMap(void) {
         auto ret_pair = sl.Insert(std::pair<int, TestValue>(i, TestValue(i)));
         assert(ret_pair.second);
     }
-
-    cout << "----- test " << __func__ << " end -----" << endl;
 }
 
 static void PrepareTestData(vector<uint32_t>* data) {
@@ -117,7 +144,7 @@ uint64_t diff_time_usec(struct timeval end, const struct timeval* begin) {
 }
 
 static void TestPerf() {
-    cout << "----- performance test begin -----" << endl;
+    cout << "----- test insert perf -----" << endl;
     vector<uint32_t> test_data;
     PrepareTestData(&test_data);
 
@@ -139,6 +166,38 @@ static void TestPerf() {
 
     cout << "skiplist insert cost " << diff_time_usec(sl_end, &sl_begin) / 1000.0 << " ms, "
          << "std::set insert cost " << diff_time_usec(st_end, &st_begin) / 1000.0 << " ms."
+         << endl;
+
+    cout << "----- test lookup perf -----" << endl;
+
+    gettimeofday(&st_begin, nullptr);
+    for (auto it : test_data) {
+        st.find(it);
+    }
+    gettimeofday(&st_end, nullptr);
+
+    gettimeofday(&sl_begin, nullptr);
+    for (auto it : test_data) {
+        sl.Lookup(it);
+    }
+    gettimeofday(&sl_end, nullptr);
+
+    cout << "skiplist lookup cost " << diff_time_usec(sl_end, &sl_begin) / 1000.0 << " ms, "
+         << "std::set lookup cost " << diff_time_usec(st_end, &st_begin) / 1000.0 << " ms."
+         << endl;
+
+    cout << "----- test traversal perf -----" << endl;
+
+    gettimeofday(&st_begin, nullptr);
+    for (auto it = st.begin(); it != st.end(); ++it);
+    gettimeofday(&st_end, nullptr);
+
+    gettimeofday(&sl_begin, nullptr);
+    for (auto it = sl.GetBeginIterator(); it != sl.GetEndIterator(); ++it);
+    gettimeofday(&sl_end, nullptr);
+
+    cout << "skiplist traversal cost " << diff_time_usec(sl_end, &sl_begin) / 1000.0 << " ms, "
+         << "std::set traversal cost " << diff_time_usec(st_end, &st_begin) / 1000.0 << " ms."
          << endl;
 }
 
