1d7b21b7219360c785a0d7245305a5e15fbf5a25
Guoyu Ou <benogy@gmail.com>
2024-10-22 21:37:06 +0800

    CircularBuffer minor changes

--- include/cpputils/circular_buffer.h
+++ include/cpputils/circular_buffer.h
@@ -10,26 +10,41 @@ template <typename T>
 class CircularBuffer final {
 public:
     CircularBuffer(uint32_t max_size) {
-        m_head = m_tail = m_size = 0;
+        m_tail = -1;
+        m_head = m_size = 0;
         m_buffer.resize(max_size);
+    }
+
+    template <typename ItemType>
+    void PushBack(ItemType&& item) {
+        m_tail = (m_tail + 1) % m_buffer.size();
+        m_buffer[m_tail] = std::forward<ItemType>(item);
+
+        if (m_size == m_buffer.size()) {
+            m_head = (m_head + 1) % m_buffer.size();
+        } else {
+            ++m_size;
+        }
     }
 
     uint32_t Size() const {
         return m_size;
     }
 
-    bool IsFull() const {
-        return (m_size == m_buffer.size());
+    T& Front() {
+        return m_buffer[m_head];
     }
 
-    void PushBack(const T& item) {
-        m_buffer[m_tail] = item;
-        AfterPushBack();
+    const T& Front() const {
+        return m_buffer[m_head];
     }
 
-    void PushBack(T&& item) {
-        m_buffer[m_tail] = std::move(item);
-        AfterPushBack();
+    T& Back() {
+        return m_buffer[m_tail];
+    }
+
+    const T& Back() const {
+        return m_buffer[m_tail];
     }
 
     T& At(uint32_t idx) {
@@ -40,17 +55,6 @@ public:
     const T& At(uint32_t idx) const {
         auto pos = (m_head + idx) % m_buffer.size();
         return m_buffer[pos];
-    }
-
-private:
-    void AfterPushBack() {
-        m_tail = (m_tail + 1) % m_buffer.size();
-
-        if (IsFull()) {
-            m_head = (m_head + 1) % m_buffer.size();
-        } else {
-            ++m_size;
-        }
     }
 
 private:
